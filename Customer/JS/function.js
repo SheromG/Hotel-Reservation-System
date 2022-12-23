a208a2b189cf74a1398ddf0646cf0bc1b695449e
SheromG <david.sherom.granada@gmail.com>
2022-12-23 21:10:28 +0800

    Add users

--- Customer/JS/function.js
+++ Customer/JS/function.js
@@ -1,18 +1,18 @@
-function signin()
-{
-    location.replace("Customer/Webpages/Dashboard.html")
-}
-function signup()
-{
-    location.replace("index.html")
-}
 function load() 
 {
     const name = document.querySelector('#name');
+    const id = document.querySelector('#id');
+    const room = document.querySelector('#room');
 
-    name.innerHTML = sessionStorage.getItem("username");
+    let firstname = sessionStorage.getItem("username");
     let lastname = sessionStorage.getItem("password");
+    let roomvar = sessionStorage.getItem("room");
+    let idvar = sessionStorage.getItem("id");
 
+    firstname = firstname.charAt(0).toUpperCase() + firstname.slice(1);
+    lastname = lastname.charAt(0).toUpperCase() + lastname.slice(1);
     
-    name.innerHTML = name.innerHTML + " " + lastname;
+    name.innerHTML = firstname + " " + lastname;
+    room.innerHTML = roomvar;
+    id.innerHTML = idvar;
 }
