6202c6f0ab3bfde16aba3d9e92652fcb91b464d9
SheromG <david.sherom.granada@gmail.com>
2022-12-23 20:01:37 +0800

    Fix name in account

--- Customer/JS/function.js
+++ Customer/JS/function.js
@@ -6,3 +6,13 @@ function signup()
 {
     location.replace("index.html")
 }
+function load() 
+{
+    const name = document.querySelector('#name');
+
+    name.innerHTML = sessionStorage.getItem("username");
+    let lastname = sessionStorage.getItem("password");
+
+    
+    name.innerHTML = name.innerHTML + " " + lastname;
+}
