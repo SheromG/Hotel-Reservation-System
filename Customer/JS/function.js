ac2000b92ccfbd6339482cb246f0d41f6dde55d1
SheromG <david.sherom.granada@gmail.com>
2022-12-23 22:07:16 +0800

    Fix dates on dashboard

--- Customer/JS/function.js
+++ Customer/JS/function.js
@@ -15,4 +15,21 @@ function load()
     name.innerHTML = firstname + " " + lastname;
     room.innerHTML = roomvar;
     id.innerHTML = idvar;
+
+    //Dashboard
+    const week = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+    const current = document.querySelector('#current');
+    const tomorrow = document.querySelector('#tomorrow');
+
+    const today = new Date();
+    const currentdate = (today).toString().split(' ').splice(1,3).join(' ');
+    const day = week[today.getDay()];
+
+    today.setDate(today.getDate() + 1);
+    const tomorrowdate = (today).toString().split(' ').splice(1,3).join(' ');
+    const day2 = week[today.getDay()];
+
+    current.innerHTML = day + ", " + currentdate;
+    tomorrow.innerHTML = day2 + ", " + tomorrowdate;
 }
