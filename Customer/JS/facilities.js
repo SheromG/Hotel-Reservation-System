426429eb287a06a990b94ab568e2574cb8aa2e60
SheromG <david.sherom.granada@gmail.com>
2022-12-26 14:21:40 +0800

    Added slidehsow

---
+++ Customer/JS/facilities.js
@@ -0,0 +1,19 @@
+let index = 0;
+
+function slideshow() 
+{
+    let x = document.getElementsByClassName("facilitiespics");
+
+    for (let i = 0; i < x.length; i++) 
+    {
+        x[i].style.display = "none";  
+    }
+
+    index++;
+
+    if (index > x.length) {index = 1} 
+    
+    x[index-1].style.display = "block";  
+
+    setTimeout(slideshow, 2000);
+}
