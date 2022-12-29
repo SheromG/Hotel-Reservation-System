fa23aaee9d290d9d96b25c167f3aa63a9dc512be
SheromG <david.sherom.granada@gmail.com>
2022-12-29 20:10:59 +0800

    Fix JavaScript

--- Customer/JS/booking.js
+++ Customer/JS/booking.js
@@ -71,3 +71,37 @@ button3.addEventListener('click', function(event)
     }, 1000);
 }, true);
 
+button1.onclick = function()
+{
+    localStorage.setItem("button11", "Current Package");
+    localStorage.setItem("button12", "current");
+    localStorage.setItem("button21", "Upgrade Package");
+    localStorage.setItem("button22", "upgrade");
+    localStorage.setItem("button31", "Upgrade Package");
+    localStorage.setItem("button32", "upgrade");
+}
+button2.onclick = function()
+{
+    localStorage.setItem("button11", "Upgrade Package");
+    localStorage.setItem("button12", "upgrade");
+    localStorage.setItem("button21", "Current Package");
+    localStorage.setItem("button22", "current");
+    localStorage.setItem("button31", "Upgrade Package");
+    localStorage.setItem("button32", "upgrade");
+}
+button3.onclick = function()
+{
+    localStorage.setItem("button11", "Upgrade Package");
+    localStorage.setItem("button12", "upgrade");
+    localStorage.setItem("button21", "Upgrade Package");
+    localStorage.setItem("button22", "upgrade");
+    localStorage.setItem("button31", "Current Package");
+    localStorage.setItem("button32", "current");
+}
+
+button1.innerHTML = localStorage.getItem("button11");
+button1.className = localStorage.getItem("button12");
+button2.innerHTML = localStorage.getItem("button21");
+button2.className = localStorage.getItem("button22");
+button3.innerHTML = localStorage.getItem("button31");
+button3.className = localStorage.getItem("button32");
